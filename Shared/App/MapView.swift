ae368900174d9e49f45d9fba8dade393b221a6e4
zestybean <brando.lugo@gmail.com>
2021-06-06 12:18:22 -0500

    Created a custom map view with custom annotations

--- Shared/App/MapView.swift
+++ Shared/App/MapView.swift
@@ -39,14 +39,55 @@ struct MapView: View {
             //MapMarker(coordinate: item.location, tint: .accentColor)
             
             // (C) BASIC ANNOTATION: (INTERACTIVE)
+            //MapAnnotation(coordinate: item.location){
+            //    Image("logo")
+            //        .resizable()
+            //        .scaledToFit()
+            //        .frame(width: 32, height: 32, alignment: .center)
+            
+            // (D) COMPLEX MAP ANNOTATION (INTERACTIVE)
             MapAnnotation(coordinate: item.location){
-                Image("logo")
+                MapAnnotationView(location: item)
+            }
+        })//:MAP
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
-            }
-        })
-        
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3){
+                    HStack{
+                        Text("Latitude")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    Divider()
+                    HStack{
+                        Text("Longitude")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }//:HSTACK
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .background(Color.black.cornerRadius(8)
+                            .opacity(0.6))
+            .padding()
+            ,alignment: .top
+        )
     }
 }
 
